25ef40886e75bc9933402ef63dbf344d94eb0f79
Yury Nemchin <ynemchin@broadvine.com>
2022-05-12 18:14:32 -0400

    OPS-381 Initial commit

---
+++ app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    return <h1>hello world</h1>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
